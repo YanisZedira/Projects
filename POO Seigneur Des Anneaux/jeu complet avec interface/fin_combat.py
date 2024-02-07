34cf08fb4199149b52e20d818a2f7b1e1fd96062
Yanis.Z <155983383+YanisZedira@users.noreply.github.com>
2024-02-07 14:06:04 +0100

    Add files via upload

---
+++ POO Seigneur Des Anneaux/jeu complet avec interface/fin_combat.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import time
+
+
+class FinCombat(tk.Toplevel):
+    def __init__(self, vainqueur):
+        super().__init__()
+        self.title("Fin du combat")
+        self.attributes('-fullscreen', True)
+
+        if vainqueur == "Magicien":
+            image_path = r"magicien.jpg"
+            texte = "Le Magicien a triomphé du maléfique Roi-Sorcier d'Angmar !"
+        else:
+            image_path = r"sorcier.webp"
+            texte = "Le Roi-Sorcier d'Angmar a vaincu le puissant Magicien Gandalf !"
+
+        image = Image.open(image_path)
+        image = image.resize((300, 300), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+
+        label_image = tk.Label(self, image=photo)
+        label_image.image = photo
+        label_image.pack()
+
+        self.label_texte = tk.Label(self, text="", font=("Arial", 20))
+        self.label_texte.pack()
+
+        self.afficher_texte(texte)
+
+    def afficher_texte(self, texte):
+        for char in texte:
+            self.label_texte.config(
+                text=self.label_texte.cget("text") + char
+            )
+            self.update()
+            time.sleep(0.1)
